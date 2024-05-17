eb36682f7499acd17ead00e30e718890f646c658
Yauhen Pahrabniak <yauhen.pahrabniak@rockstargames.com>
2024-05-17 14:23:35 +0200

    Params update

--- build/prepare-depot.ps1
+++ build/prepare-depot.ps1
@@ -1,6 +1,7 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-Set-Location -Path "..\depot_tools"
+$CURRENT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location -Path "${CURRENT_DIR}\..\depot_tools"
 
 Remove-Item -Path ".cipd_bin", ".cipd_client_cache", ".versions", "external_bin" -Recurse -Force
 Remove-Item -Path ".cipd_client.exe", "gclient_utils.pyc", "git-bash", "git.bat", "gitk.bat", "metrics.cfg", "python_bin_reldir.txt", "python.bat", "python3_bit_reldir.txt", "python3.bat", "ssh-keygen.bat", "sh.bat", "subrpocess2.pyc" -Force
@@ -9,4 +10,6 @@ Set-Location -Path "bootstrap"
 
 & ".\win_tools.bat"
 
-Set-Location -Path "..\..\build"
+$env:PATH = "${CURRENT_DIR}\..\depot_tools;" + $env:PATH
+
+Set-Location -Path "${CURRENT_DIR}\.."
